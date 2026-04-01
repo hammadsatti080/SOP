a94b290aa69296eb7da5775124df526c6f3fc54f
hammadsatti080 <abdullahsatti98712@gmail.com>
2026-04-01 12:14:24 +0500

    Add remaining files

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -165,7 +165,7 @@ function Login() {
     if (!validate()) return;
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("sop-backend-project.vercel.app/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
